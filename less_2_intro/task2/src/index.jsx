8cdc1da9608edb69b2efe406d64d3079d9b510ae
karrmao <karrmao@gmail.com>
2022-12-23 20:29:41 +0200

    add

---
+++ less_2_intro/task2/src/index.jsx
@@ -0,0 +1,38 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+export const increment = () => {
+  return { type: INCREMENT };
+};
+
+export const decrement = () => {
+  return { type: DECREMENT };
+};
+
+export const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+export const store = createStore(counterReducer);
+
+//- test data
+
+//console.log(store); //{dispatch: ƒ, subscribe: ƒ, getState: ƒ, replaceReducer: ƒ, @@observable: ƒ}
+
+// store.dispatch(increment()); //1
+// store.dispatch(increment()); //2
+// store.dispatch(increment()); //3
+// store.dispatch(decrement()); //2
+
+// console.log(store.getState()); // 0
+
+//console.log(store.getState()); // {name: 'Tom'} if state = { name: 'Tom' }

9f18bdc70a81301741bf9bdebc0ed004cecc070a
karrmao <karrmao@gmail.com>
2022-12-23 19:55:22 +0200

    add

---
+++ less_1_context/task2/src/UserMenu.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { UserDataContex } from './userData-context';
+
+class UserMenu extends Component {
+  render() {
+    return (
+      <div className="menu">
+        <span className="menu__greeting">{`Hello, ${this.context.name}`}</span>
+        <img
+          alt="User Avatar"
+          src={this.context.avatar_url}
+          className="menu__avatar"
+        />
+      </div>
+    );
+  }
+}
+
+UserMenu.contextType = UserDataContex;
+export default UserMenu;
--- less_1_context/task2/src/index.jsx
+++ less_1_context/task2/src/index.jsx
@@ -1,30 +1,8 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.scss';
-// import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import App from './App';
 
-// const rootElement = document.querySelector('#root');
+const rootElement = document.querySelector('#root');
 
-// ReactDOM.render(<App />, rootElement);
-import { createStore } from 'rdux';
-
-const incrementAction = {
-  type: 'INCREMENT',
-};
-
-const decrementAction = {
-  type: 'DECREMENT',
-};
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
-  }
-};
-
-const store = createReducer();
+ReactDOM.render(<App />, rootElement);

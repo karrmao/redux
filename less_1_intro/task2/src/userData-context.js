943f209bd4ec40d0257187e3bba11ebe0c638002
karrmao <karrmao@gmail.com>
2022-12-23 19:53:43 +0200

    add

--- less_1_intro/task2/src/userData-context.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const userData = {
-  name: 'Nikola Tesla',
-  avatar_url: 'https://avatars3.githubusercontent.com/u10001',
-};
-
-export default userData;
-export const UserDataContex = React.createContext();
---
+++ less_2_intro/task1/src/themes-context.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const themes = {
+  dark: {
+    fontColor: '#ffffff', //white
+    background: '#222222', //ble
+  },
+  light: {
+    fontColor: '#000000', //black
+    background: '#eeeeee', //silver
+  },
+};
+
+export const ThemeContext = React.createContext(themes.dark);

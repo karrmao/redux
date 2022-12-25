2401e2529ba809c939f97d4b05e8229a6a3556f5
karrmao <karrmao@gmail.com>
2022-12-25 22:04:03 +0200

    add

--- less_3_combinin_reducer/task1/src/index.js
+++ less_3_combinin_reducer/task1/src/index.js
@@ -10,9 +10,7 @@ import {
   updateUser,
 } from './users.actions';
 
-store.subscribe(() =>
-  console.log(store.getState()),
-);
+store.subscribe(() => console.log(store.getState()));
 
 store.dispatch(increment());
 store.dispatch(decrement());
@@ -21,8 +19,6 @@ store.dispatch(increment());
 store.dispatch(reset());
 
 store.dispatch(addUser({ id: 1, name: 'Tom' }));
-store.dispatch(
-  addUser({ id: 2, name: 'Vinton' }),
-);
+store.dispatch(addUser({ id: 2, name: 'Vinton' }));
 
 store.dispatch(deleteUser(1));

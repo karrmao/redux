48cb0a3a0779e6d1b2975976cf6fe37dc00d1165
karrmao <karrmao@gmail.com>
2022-12-26 15:34:50 +0200

    add

--- less_3_combinin_reducer/task3/src/index.js
+++ less_3_combinin_reducer/task3/src/index.js
@@ -5,14 +5,13 @@ import { setUser } from './user.actions';
 
 store.subscribe(() => console.log(store.getState()));
 
-// store.dispatch(setLanguage('jp'));
-// store.dispatch(addProduct({ id: 76, name: 'milk' }));
-// console.log('REMOVE');
-// store.dispatch(removeProduct(76));
-// console.log('ADD');
-// store.dispatch(setUser({ name: 'Bob' }));
-// store.dispatch(setUser({ name: 'Bobby' }));
-// console.log('ADD Potato');
-// store.dispatch(addProduct({ id: 10, name: 'Peter' }));
-// store.dispatch(setLanguage('pl'));
-// store.dispatch(setUser({ name: 'Anjey' }));
+store.dispatch(setLanguage('jp'));
+store.dispatch(addProduct({ id: 76, name: 'milk' }));
+console.log('REMOVE');
+store.dispatch(removeProduct(76));
+console.log('ADD');
+store.dispatch(setUser({ name: 'Bob' }));
+console.log('ADD Potato');
+store.dispatch(addProduct({ id: 10, name: 'Peter' }));
+store.dispatch(setLanguage('pl'));
+store.dispatch(setUser({ name: 'Anjey' }));

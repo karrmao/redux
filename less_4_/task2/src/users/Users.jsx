30d457c51d1ed203c54cb975559fc8dc06258e20
karrmao <karrmao@gmail.com>
2022-12-27 20:55:40 +0200

    add

--- less_4_/task2/src/users/Users.jsx
+++ less_4_/task2/src/users/Users.jsx
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 class Users extends Component {
   render() {
+    const { users } = this.props;
     <div className="users">
-      <button className="users__create-btn"></button>
+      <button className="users__create-btn">Create Batton</button>
+      <ul className="users__list">
+        {users.map(user => (
+          <li className="users__list-item"></li>
+        ))}
+      </ul>
     </div>;
   }
 }
+
+export default Users;

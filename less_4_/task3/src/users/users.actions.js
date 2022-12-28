3b19f99207926776d6e5e4a568ef95e6dc51263f
karrmao <karrmao@gmail.com>
2022-12-28 21:24:08 +0200

    add

--- less_4_/task3/src/users/users.actions.js
+++ less_4_/task3/src/users/users.actions.js
@@ -1,10 +1,12 @@
-export const GO_NEXT = 'USERSLIST/GO_NEXT';
-export const GO_PREV = 'USERSLIST/GO_PREV';
+export const TO_NEXT_PAGE =
+  'USERSLIST/TO_NEXT_PAGE';
+export const TO_PREV_PAGE =
+  'USERSLIST/TO_PREV_PAGE';
 
 export const goNext = () => {
-  return { type: GO_NEXT };
+  return { type: TO_NEXT_PAGE };
 };
 
-export const goPrev = userId => {
-  return { type: GO_PREV };
+export const goPrev = () => {
+  return { type: TO_PREV_PAGE };
 };

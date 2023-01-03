4f201331783453a51b0abc3ee9641e11464c7fd5
karrmao <karrmao@gmail.com>
2023-01-03 23:56:56 +0200

    add

--- less_6/task2/src/users/SearchField.jsx
+++ less_6/task2/src/users/SearchField.jsx
@@ -2,7 +2,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import * as usersActions from './user.actions';
+import * as usersActions from './users.actions';
 
 class SearchField extends Component {
   state = {

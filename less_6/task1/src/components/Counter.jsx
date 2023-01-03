b233e1116110730d61ec0f9b2eb9858d1fb9f0c6
karrmao <karrmao@gmail.com>
2023-01-03 16:07:38 +0200

    add

---
+++ less_6/task1/src/components/Counter.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+// import { increment, decrement, reset } from '../counter.actions';
+import * as counterActions from '../counter.actions';
+
+// const Counter = ({ counter, incr, decr, res }) => {
+const Counter = ({ counter, increment, decrement, reset }) => {
+  return (
+    <div className="counter">
+      {/* <button className="counter__button" onClick={incr}> */}
+      <button className="counter__button" onClick={decrement}>
+        -
+      </button>
+      {/* <span className="counter__value" onClick={decr}> */}
+      <span className="counter__value" onClick={reset}>
+        {counter}
+      </span>
+      {/* <button className="counter__button" onClick={res}> */}
+      <button className="counter__button" onClick={increment}>
+        +
+      </button>
+    </div>
+  );
+};
+const mapState = state => {
+  return {
+    counter: state,
+  };
+};
+
+// const mapDispatch = dispatch => {
+//   return {
+//     incr: () => dispatch(increment()),
+//     decr: () => dispatch(decrement()),
+//     res: () => dispatch(reset()),
+//   };
+// };
+
+const mapDispatch = {
+  increment: counterActions.increment,
+  decrement: counterActions.decrement,
+  reset: counterActions.reset,
+};
+const connector = connect(mapState, mapDispatch); //HOC
+
+export default connector(Counter);

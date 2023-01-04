7880944cfd5f3d56244cade3e5bd2a0dbe7e5d94
karrmao <karrmao@gmail.com>
2023-01-04 12:31:32 +0200

    add

--- less_6/task2/src/users/users.actions.js
+++ less_6/task2/src/users/users.actions.js
@@ -1,6 +1,3 @@
-/* eslint-disable spaced-comment */
-/* eslint-disable import/prefer-default-export */
-/* eslint-disable arrow-body-style */
 export const SHOW_SPINNER = 'SHOW_SPINNER';
 export const USER_DATA_RECIEVED = 'USER_DATA_RECIEVED';
 import { getUserData } from './users.gateway';
@@ -24,9 +21,7 @@ export const fetchUserData = userName => {
   return function (dispatch) {
     dispatch(showSpinner());
     getUserData(userName).then(userData => {
-      //console.log(userData);
       dispatch(userDataRecieved(userData));
     });
   };
 };
-//console.log('I am async action');

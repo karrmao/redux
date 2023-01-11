c069c8b472613668ff8f8a7527da00fc3318aafd
karrmao <karrmao@gmail.com>
2023-01-11 20:32:40 +0200

    add

--- less_7/todo_list_part_zero/src/tasks/tasksGateway.jsx
+++ less_7/todo_list_part_zero/src/tasks/tasksGateway.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable arrow-body-style */
-
-const baseUrl = 'https://63b6f97a4f17e3a931c6a0d0.mockapi.io/api/v1/tasks';
+// const baseUrl = 'https://63b6f97a4f17e3a931c6a0d0.mockapi.io/api/v1/tasks';
+const baseUrl = 'https://619a8b0a27827600174452a1.mockapi.io/api/v1/tasks';
 
 export const createTask = taskData => {
   return fetch(baseUrl, {

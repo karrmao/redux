d9ea41cbe94a3135ef2022bdf2cb49fcb161972c
karrmao <karrmao@gmail.com>
2023-01-22 13:29:34 +0200

    add

--- less_4_/task2/src/store.js
+++ less_4_/task2/src/store.js
@@ -1,18 +1,12 @@
-import {
-  createStore,
-  combineReducers,
-} from 'redux';
-//import counterReduser from './counter/counter.reducer';
+/* eslint-disable spaced-comment */
+import { createStore, combineReducers } from 'redux';
 import usersReduser from './users/users.reducer';
 
 const reducer = combineReducers({
   users: usersReduser,
-  // counter: counterReduser,
 });
-
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__(),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 export default store;
--- less_4_/task2/src/users/Users.jsx
+++ less_4_/task2/src/users/Users.jsx
@@ -1,12 +1,11 @@
+/* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as userActions from './users.actions';
 
 class Users extends Component {
   onUserCreate = () => {
-    const id = Math.round(
-      Math.random() * 1000000,
-    );
+    const id = Math.round(Math.random() * 1000000);
     const newUser = {
       id,
       name: `User name # ${id}`,
@@ -16,27 +15,18 @@ class Users extends Component {
 
   render() {
     const { users, deleteUser } = this.props;
-
     return (
       <div className="users">
-        <button
-          onClick={this.onUserCreate}
-          className="users__create-btn"
-        >
+        <button onClick={this.onUserCreate} className="users__create-btn">
           Create User
         </button>
         <ul className="users__list">
           {users.map(user => (
-            <li
-              key={user.id}
-              className="users__list-item"
-            >
+            <li key={user.id} className="users__list-item">
               {user.name}
               <button
                 className="users__delete-btn"
-                onClick={() =>
-                  deleteUser(user.id)
-                }
+                onClick={() => deleteUser(user.id)}
               >
                 +
               </button>
@@ -59,7 +49,5 @@ const mapDispatch = {
   deleteUser: userActions.deleteUser,
 };
 const connector = connect(mapState, mapDispatch);
-
 const ConnectedUsers = connector(Users);
-
 export default ConnectedUsers;

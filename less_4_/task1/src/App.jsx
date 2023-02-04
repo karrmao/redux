339a50ea0264baed891449adb65994904c04e169
karrmao <karrmao@gmail.com>
2023-02-04 20:43:16 +0200

    add

--- less_4_/task1/src/App.jsx
+++ less_4_/task1/src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 import Counter from './components/Counter';
-
-store.subscrible(() => console.log(store.getState()));
-
+/*************************************************/
+store.subscribe(() => console.log(store.getState()));
+/*************************************************/
 const App = () => {
   return (
     <Provider store={store}>

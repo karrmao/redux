5cc9de2bad9221d708b9ba1ccd2858a9be35c536
karrmao <karrmao@gmail.com>
2023-02-04 17:48:34 +0200

    add

--- less_3_combinin_reducer/task1/src/store.js
+++ less_3_combinin_reducer/task1/src/store.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-underscore-dangle */
 import { createStore, combineReducers } from 'redux';
 import { counterReducer } from './counter.reducer';
-import { usersReducer } from './users.reducer';
+import { usersReduser } from './users.reducer';
 
 const appReducer = combineReducers({
   counter: counterReducer,
-  users: usersReducer,
+  users: usersReduser,
 });
 const store = createStore(
   appReducer,
